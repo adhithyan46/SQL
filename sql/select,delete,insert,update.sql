63da72a682eab0ffebbc717737454d8a0bc0fe88
adhithyan46 <ksadhithyan100@gmail.com>
2024-06-24 13:20:15 +0530

    Add files via upload

---
+++ sql/select,delete,insert,update.sql
@@ -0,0 +1,10 @@
+select firstname from persons
+
+delete from persons where firstname="babu"
+
+insert persons(firstname,place,lastname)
+values('adhi','kzk','ks');
+insert persons(firstname,place,lastname)
+values('shibu','kkd','kb');
+insert persons(firstname,place,lastname)
+values('babu','kvk','bb');
