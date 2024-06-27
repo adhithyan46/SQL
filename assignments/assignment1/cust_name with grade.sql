ff53a80b6abcb754c811a99fb512f496401b724f
adhithyan46 <ksadhithyan100@gmail.com>
2024-06-27 18:18:23 +0530

    Delete assignments/assignment1 directory

--- assignments/assignment1/cust_name with grade.sql
+++
@@ -1,5 +0,0 @@
-select customer.customer_id,customer.cust_name,customer.grade
-from customer
-join orders on customer.customer_id=orders.customer_id
-join salesman on customer.salesman_id=salesman.salesman_id
-where customer.city is not null and customer.grade is not null
--- assignments/assignment1/cust_name with ord_no.sql
+++
@@ -1,4 +0,0 @@
-select customer.cust_name ,orders.ord_no 
-from customer
-inner join orders on customer.customer_id= orders.customer_id
-
